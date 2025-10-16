3cd0f59f7e4376feb727054833667e22dfc37991
mafdysaad <mafdysaad366@gmail.com>
2025-10-17 02:24:58 +0300

    finsh home repoimplmintaion

--- lib/Features/home/Data/repo/homerepoimplmantion.dart
+++ lib/Features/home/Data/repo/homerepoimplmantion.dart
@@ -1,44 +1,28 @@
-import 'package:booklyapp/Features/home/Data/models/book_model/book_modle.dart';
+import 'package:booklyapp/Features/home/Data/data_sources/home_remot_data_source.dart';
+import 'package:booklyapp/Features/home/Data/data_sources/localdatatsource.dart';
+
+import 'package:booklyapp/Features/home/Domain/entites/entites.dart';
 import 'package:booklyapp/Features/home/Domain/home_repo/home_repo.dart';
 
 import 'package:booklyapp/core/error/error.dart';
-import 'package:booklyapp/core/utils/api_services.dart';
+
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
 class Homerepoimplmantion extends repo {
-  final Apiservices apiservices;
+  final Remotdatasource remotdatasource;
+  final Localdatatsource localdatatsource;
 
-  Homerepoimplmantion(this.apiservices);
+  Homerepoimplmantion(this.localdatatsource, this.remotdatasource);
   @override
-  Future<Either<failuer, List<BookModle>>> featchBooks() async {
+  Future<Either<failuer, List<BookEntites>>> featchBooks() async {
     try {
-      var data = await apiservices.get('volumes?q=programming');
-      List<BookModle> books = [];
-      for (var item in data['items']) {
-        books.add(BookModle.fromJson(item));
+      List<BookEntites> books;
+      books = localdatatsource.featchBooks();
+      if (books.isNotEmpty) {
+        return right(books);
       }
-      return Right(books);
-    } on Exception catch (e) {
-      if (e is DioException) {
-        return left(serverfailuer.ForDioExcption(e));
-      } else {
-        return left(serverfailuer(e.toString()));
-      }
-    }
-  }
-
-  @override
-  Future<Either<failuer, List<BookModle>>> searchBook(
-      {required String bookName}) async {
-    try {
-      var respons = await apiservices
-          .get('volumes?q=$bookName|"title"=$bookName&filtering=free-ebooks');
-
-      List<BookModle> books = [];
-      for (var item in respons['items']) {
-        books.add(BookModle.fromJson(item));
-      }
+      books = await remotdatasource.featchBooks();
       return right(books);
     } on Exception catch (e) {
       if (e is DioException) {
@@ -50,17 +34,36 @@ class Homerepoimplmantion extends repo {
   }
 
   @override
-  Future<Either<failuer, List<BookModle>>> relatedBook(
+  Future<Either<failuer, List<BookEntites>>> searchBook(
+      {required String bookName}) async {
+    try {
+      List<BookEntites> books;
+      books = localdatatsource.searchBook(bookName: bookName);
+      if (books.isNotEmpty) {
+        return right(books);
+      }
+      books = await remotdatasource.searchBook(bookName: bookName);
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(serverfailuer.ForDioExcption(e));
+      } else {
+        return left(serverfailuer(e.toString()));
+      }
+    }
+  }
+
+  @override
+  Future<Either<failuer, List<BookEntites>>> relatedBook(
       {required String categories}) async {
     try {
-      var respons = await apiservices
-          .get('volumes?q=$categories&filtering=free-ebooks&Sorting=relevance');
-
-      List<BookModle> books = [];
-      for (var item in respons['items']) {
-        books.add(BookModle.fromJson(item));
+      List<BookEntites> books;
+      books = localdatatsource.relatedBook(categories: categories);
+      if (books.isNotEmpty) {
+        return right(books);
       }
-      return Right(books);
+      books = await remotdatasource.relatedBook(categories: categories);
+      return right(books);
     } on Exception catch (e) {
       if (e is DioException) {
         return left(serverfailuer.ForDioExcption(e));
